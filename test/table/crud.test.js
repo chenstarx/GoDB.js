c6ec36a30073f5dd4f6e36f00c94642a1a4fb9b3
chenstarx <chenstarx@outlook.com>
2021-01-16 00:24:27 +0800

    test: simpler crud test code

--- test/table/crud.test.js
+++ test/table/crud.test.js
@@ -1,51 +1,53 @@
-function crudTest(dbName) {
-  if (!dbName) return alert('database name required');
+const schema = {
+  user: {
+    name: {
+      type: String,
+      unique: true
+    },
+    age: Number
+  }
+};
 
-  const schema = {
-    user: {
-      name: {
-        type: String,
-        unique: true
-      },
-      age: Number
-    }
-  };
+const db = new Godb('testDB', schema);
+const user = db.table('user');
 
-  const db = new Godb(dbName, schema);
+// Create
+user.add({
+  name: 'elain',
+  age: 23
+}).then((id) => {
+  console.log('\n');
+  console.log('elain added with id:', id);
+});
 
-  const user = db.table('user');
+crud();
 
-  user.add({
+async function crud() {
+
+  // Create:
+  await user.add({
     name: 'luke',
     age: 22
-  })
-    .then(async () => {
-      console.log('\n');
-      console.log('add user luke:');
-      await user.consoleTable();
+  });
 
-      console.log('\n');
-      console.log('add user elain:');
-      await user.add({
-        name: 'elain',
-        age: 24
-      });
+  console.log('add user: luke');
+  await user.consoleTable(); // show table data in console
 
-      await user.consoleTable();
+  // Read:
+  const luke = await user.get({ name: 'luke' });
+  // const luke = await user.get({ id: 1 });
 
-      console.log('\n');
-      console.log('put luke, set luke.age to 23:');
-      luke = await user.get({ name: 'luke' });
-      luke.age = 23;
-      await user.put(luke);
+  // Update:
+  luke.age = 23;
+  await user.put(luke);
 
-      await user.consoleTable();
+  console.log('update: set luke.age to 23');
+  await user.consoleTable();
 
-      console.log('\n');
-      console.log('delete user luke:');
-      await user.delete({ name: 'luke' });
-      await user.consoleTable();
+  // Delete:
+  await user.delete({ name: 'luke' });
 
-    })
-    .catch(err => console.error(err));
+  console.log('delete user: luke');
+  await user.consoleTable();
+
 }
