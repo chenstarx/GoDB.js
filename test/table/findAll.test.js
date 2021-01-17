0b1ec1e2e1b61d29bd67422e9eb643cc269bea6e
chenstarx <chenstarx@outlook.com>
2021-01-17 20:43:46 +0800

    test: Table.findAll()

---
+++ test/table/findAll.test.js
@@ -0,0 +1,49 @@
+function findAllTest(dbName) {
+  if (!dbName) return alert('database name required');
+
+  const schema = {
+    user: {
+      name: {
+        type: String,
+        unique: true
+      },
+      age: Number
+    }
+  };
+
+  const db = new Godb(dbName, schema);
+  const user = db.table('user');
+
+  crud();
+
+  async function crud() {
+
+    // Create:
+    const data = [];
+
+    for (let i = 0; i < 300; i++) {
+      data.push({
+        name: `luke${i}`,
+        age: Math.floor(Math.random() * 100)
+      })
+    }
+
+    await user.addMany(data);
+
+    await user.consoleTable();
+
+    // Find:
+    result = await user.findAll(item => item.name === 'luke');
+
+    console.log(`find result for 'luke': `, result);
+
+    result = await user.findAll(item => item.name === 'luke99');
+
+    console.log(`find result for 'luke99': `, result);
+
+    result = await user.findAll(item => item.age > 50);
+
+    console.log(`find result for age > 50: `, result);
+
+  }
+}
