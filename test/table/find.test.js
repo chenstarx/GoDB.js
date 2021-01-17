a7fcbf0c4d446a26ee1ba08d23332348bf6b96e3
chenstarx <chenstarx@outlook.com>
2021-01-17 18:49:16 +0800

    test: Table.find()

---
+++ test/table/find.test.js
@@ -0,0 +1,65 @@
+function findTest(dbName) {
+  if (!dbName) return alert('database name required');
+
+  const schema = {
+    user: {
+      name: {
+        type: String,
+        unique: true
+      },
+      age: Number
+    }
+  };
+
+  const db = new Godb(dbName, schema);
+  const user = db.table('user');
+
+  crud();
+
+  async function crud() {
+
+    // Create:
+    const data = [];
+
+    for (let i = 0; i < 100; i++) {
+      data.push({
+        name: `luke${i}`,
+        age: Math.floor(Math.random() * 100)
+      })
+    }
+
+    await user.addMany(data);
+
+    await user.consoleTable(); // show table data in console
+
+    // Find:
+    result = await user.find(item => item.name === 'luke12');
+
+    console.log(`find result for 'luke12': `, result);
+
+    result = await user.find(item => item.name === 'luke99');
+
+    console.log(`find result for 'luke99': `, result);
+
+    result = await user.find(item => item.name === 'luke0');
+
+    console.log(`find result for 'luke0': `, result);
+
+    result = await user.find(item => item.name === 'luke');
+
+    console.log(`find result for 'luke': `, result);
+
+    result = await user.find(item => item.age === 22);
+
+    console.log(`find result for age 22: `, result);
+
+    result = await user.find(item => item.age === 55);
+
+    console.log(`find result for age 55: `, result);
+
+    result = await user.find(item => item.age === 12);
+
+    console.log(`find result for age 12: `, result);
+
+  }
+}
