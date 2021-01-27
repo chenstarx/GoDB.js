a955655122fafc5e4ee47489b6a8d45b2f106b45
chenstarx <chenstarx@outlook.com>
2021-01-27 12:31:44 +0800

    test: addMany

---
+++ test/table/addMany.test.js
@@ -0,0 +1,39 @@
+function addManyTest(dbName) {
+  if (!dbName) return alert('database name required');
+
+  const schema = {
+    user: {
+      name: {
+        type: String,
+        unique: true
+      },
+      age: Number
+    }
+  };
+
+  const data = [];
+
+  const db = new Godb(dbName, { schema });
+  const user = db.table('user');
+
+  for(let i = 0; i < 10000; i++) {
+    data.push({
+      name: `user${i}`,
+      age: i % 100
+    });
+  }
+
+  adding();
+
+  async function adding() {
+
+    console.log('timer started');
+    console.time('adding');
+
+    // Create:
+    await user.addMany(data);
+
+    console.timeEnd('adding');
+    console.log('timer ended');
+  }
+}
